0d2aab8c7373d86ddc50876f67f43f614943fb19
julyperfect07 <abdallah.july05@gmail.com>
2024-04-17 11:00:31 +0300

    not found page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,13 +1,17 @@
+import { notFound } from "next/navigation";
 import React from "react";
 
 interface Params {
   params: {
-    productId: String;
-    reviewId: String;
+    productId: string;
+    reviewId: string;
   };
 }
 
 const ReviewDetail = ({ params }: Params) => {
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  }
   return (
     <h1>
       Review {params.reviewId} for product {params.productId}
