71f86bcc0517bfb02e9e1e3ab324d93d60c9bb9c
julyperfect07 <abdallah.july05@gmail.com>
2024-04-19 18:32:04 +0300

    title metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,11 @@
 import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    absolute: "Blog",
+  },
+};
 
 export default function Blog() {
   return <h1>Blog page</h1>;
