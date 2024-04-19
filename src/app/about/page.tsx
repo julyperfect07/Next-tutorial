109e53c638809900c92dfe987c8d13bdcee7b04a
julyperfect07 <abdallah.july05@gmail.com>
2024-04-19 18:16:46 +0300

    static and dynamic metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+export const metadata = {
+  title: "About codevolution",
+};
+
 export default function About() {
   return <h1>About page</h1>;
 }
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { Metadata } from "next";
+import { resolve } from "path";
 
 interface Params {
   params: {
     productId: string;
   };
 }
+
+export const generateMetadata = async ({
+  params,
+}: Params): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${params.productId}`);
+    }, 100);
+  });
+  return {
+    title: ` ${title} `,
+  };
+};
 
 export default function DetailedProduct({ params }: Params) {
   return <h1>Details about product {params.productId} </h1>;
