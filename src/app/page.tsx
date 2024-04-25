9edf8d7ba7102f125bbded10e7629bcb7b9e801a
julyperfect07 <abdallah.july05@gmail.com>
2024-04-25 13:25:46 +0300

    Navigation LInks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
 function Home() {
-  return <h1>Home page</h1>;
+  return (
+    <>
+      <h1>Home page</h1>
+      <Link href="/blog">Blog</Link>
+      <Link href="/products">Products</Link>
+    </>
+  );
 }
 
 export default Home;
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,28 @@
+import Link from "next/link";
 import React from "react";
 
 export default function ProductsPage() {
+  const productId = 100;
   return (
     <>
+      <Link href="/">Home</Link>
       <h1>List of products</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <h2>
+        <Link href="products/1">Product 1</Link>
+      </h2>
+      <h2>
+        <Link href="products/2">Product 2</Link>
+      </h2>
+      <h2>
+        <Link href="products/3" replace>
+          Product 3
+        </Link>
+      </h2>
+      <h2>
+        <Link href={`products/${productId}`}>
+          Product {productId}
+        </Link>
+      </h2>
     </>
   );
 }
